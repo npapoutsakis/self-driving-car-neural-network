a366d94fa8128b79ab81104808210482193b5885
Nikolaos Papoutsakis <npapoutsakis@ISC.TUC.GR>
2024-04-04 11:16:11 +0300

    init v2

---
+++ main.py
@@ -0,0 +1,20 @@
+#   Autonomous Agents Project 2023
+#       Nikolaos Papoutsakis
+#           2019030206
+
+
+#  Project: Self-Driving AI using NEAT
+
+
+import pygame
+import neat
+import os
+import math
+import sys
+
+
+
+
+
+if __name__ == '__main__':
+    print("Running Application")
